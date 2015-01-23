86f95fb5ca0870533f09a71a72f937231a619ffc
Naveed Ahmad <naveedahmada036@gmail.com>
2015-01-23 06:12:32 +0500

    shouldn't append / if text isn't a instagram url

--- lib/auto_html/filters/instagram.rb
+++ lib/auto_html/filters/instagram.rb
@@ -2,9 +2,9 @@ require 'uri'
 require 'net/http'
 
 AutoHtml.add_filter(:instagram) do |text|
-  text << '/' unless text.end_with?('/')
   regex = %r{https?:\/\/(www.)?instagr(am\.com|\.am)/p/.+}
   text.gsub(regex) do
-    %{<iframe src="#{text}embed" height="714" width="616" frameborder="0" scrolling="no"></iframe>}
+    embed_url = text.end_with?('/') ? text : text+'/'
+    %{<iframe src="#{embed_url}embed" height="714" width="616" frameborder="0" scrolling="no"></iframe>}
   end
 end
