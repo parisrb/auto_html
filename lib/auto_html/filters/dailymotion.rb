32cdce4d3195d213ceac9a345eceecdecbf4004b
Naveed Ahmad <naveedahmada036@gmail.com>
2016-05-03 07:15:57 +0500

    removed with & height from url

--- lib/auto_html/filters/dailymotion.rb
+++ lib/auto_html/filters/dailymotion.rb
@@ -2,6 +2,9 @@ AutoHtml.add_filter(:dailymotion).with(:width => 480, :height => 360) do |text, 
   text.gsub(/http:\/\/www\.dailymotion\.com.*\/video\/(.+)_*/) do
     video_id = $1
     options ||= {autoplay: 0}
+    width = options.delete :width
+    height = options.delete :height
+
     url_options = options.map { |k, v| "#{k}=\"#{v}\"" }.join('&')
 
     "<iframe src='//www.dailymotion.com/embed/video/#{video_id}?#{url_options}' frameborder='0'></iframe>"
